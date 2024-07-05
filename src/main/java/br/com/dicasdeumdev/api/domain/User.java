c6568c83019545d0b755bc1159b36e9538ea8137
sabrinagalvan <sabrinagalvans2@gmail.com>
2024-07-05 20:54:29 -0300

    classe user

---
+++ src/main/java/br/com/dicasdeumdev/api/domain/User.java
@@ -0,0 +1,24 @@
+package br.com.dicasdeumdev.api.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
