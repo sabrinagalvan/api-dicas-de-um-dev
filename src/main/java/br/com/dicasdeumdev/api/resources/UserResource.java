41d98f57ef456b86769fd3863a279410662d9fee
sabrinagalvan <sabrinagalvans2@gmail.com>
2024-07-05 21:39:01 -0300

    classe user resource findById

---
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -0,0 +1,20 @@
+package br.com.dicasdeumdev.api.resources;
+
+import br.com.dicasdeumdev.api.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findyById(@PathVariable Integer id) {
+
+        return ResponseEntity.ok().body(new User(1, "Sabrina", "bri@mail.com", "123"));
+    }
+
+}
